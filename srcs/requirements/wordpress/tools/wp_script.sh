ac97f964e40882eec36849f3207f65fa999317bd
fatima_zahra louta <fatimazahralouta@gmail.com>
2023-07-07 14:59:57 +0000

    fixed php-fpm not  found error in wordpres image

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -6,4 +6,5 @@ wp core config --dbhost="localhost" --dbname="$DB_NAME" --dbuser="$DB_USER1"  --
 # Modifies the PHP-FPM configuration file to change the listen directive to use port 9000 instead of a Unix socket
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php81/php-fpm.d/www.conf
 
-php-fpm7 -F
+exec "$@"
+~
