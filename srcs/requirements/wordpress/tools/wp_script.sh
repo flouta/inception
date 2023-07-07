b100110832d61220975ba194c6090695ff25da22
fatima_zahra louta <fatimazahralouta@gmail.com>
2023-07-07 11:55:22 +0000

    added wordpress image configuration

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-RUN wp core config --dbhost=mariadb --dbname=mydatabase --dbuser=user1 --dbpass=password1 << PHP
+# # Configure WordPress to connect to the MariaDB database
+wp core config --dbhost=mariadb --dbname="$DB_NAME" --dbuser="$DB_USER1"  --dbpass="$DB_PASSWORD1" 
 
-PHP
+# Modifies the PHP-FPM configuration file to change the listen directive to use port 9000 instead of a Unix socket
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-php-fpm7 -F
-
+exec "$@"
