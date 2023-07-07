ee9ed0103ff9da0ef540e52307fb37d4c6c2a3ee
fatima_zahra louta <fatimazahralouta@gmail.com>
2023-07-07 20:09:26 +0000

    fixed the connection between nginx and wordpresss containers

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+
 # Configure WordPress to connect to the MariaDB database
 wp core config --dbhost="$DATA_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER1"  --dbpass="$DB_PASSWORD1" 
 
 # Modifies the PHP-FPM configuration file to change the listen directive to use port 9000 instead of a Unix socket
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php81/php-fpm.d/www.conf
+sed -i 's|listen = /run/php/php8.1-fpm.sock|listen = 9000|g' /etc/php81/php-fpm.d/www.conf
+sed -i 's|listen = 127.0.0.1:9000|listen = 0.0.0.0:9000|g' /etc/php81/php-fpm.d/www.conf
+
 
 exec "$@"
