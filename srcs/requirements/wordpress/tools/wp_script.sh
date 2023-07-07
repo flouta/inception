7e7a3d7d5f81722ad6995945d9b30fda903e6515
fatima_zahra louta <fatimazahralouta@gmail.com>
2023-07-07 19:14:19 +0000

    implemented docker-compose.yml file

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Configure WordPress to connect to the MariaDB database
-wp core config --dbhost="localhost" --dbname="$DB_NAME" --dbuser="$DB_USER1"  --dbpass="$DB_PASSWORD1" 
+wp core config --dbhost="$DATA_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER1"  --dbpass="$DB_PASSWORD1" 
 
 # Modifies the PHP-FPM configuration file to change the listen directive to use port 9000 instead of a Unix socket
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php81/php-fpm.d/www.conf
 
 exec "$@"
-~
