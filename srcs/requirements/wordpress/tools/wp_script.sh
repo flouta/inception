da90aab66405e2521f3133cf41f22fc3c3899a3a
fatima_zahra louta <fatimazahralouta@gmail.com>
2023-07-08 11:08:26 +0000

    added wordpress users to .env file

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 
-# mkdir /var/www/
-# mkdir /var/www/html
+mkdir /var/www/
+mkdir /var/www/html
 
-# cp /wordpress/wp-config-sample.php /var/www/html/wp-config.php
-
-# wp core install --title="idk" --admin_name="$DB_USER1" --admin_password="$DB_PASSWORD1" --admin_email="flouta@example.com"
+cp /wordpress/wp-config-sample.php /wordpress/wp-config.php
 
 # Configure WordPress to connect to the MariaDB database
-wp core config --dbhost="$DATA_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER1"  --dbpass="$DB_PASSWORD1"
+wp config create --allow-root --dbname="${DATABASE_NAME}" \
+    --dbuser="${DB_USER}" \
+    --dbpass="${DB_USER_PASS}" \
+    --dbhost="${DATABASE_HOST}" \
+    --path="/wordpress"
 
+# Install WordPress
+wp core install --allow-root --url="${WP_URL}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${ADMIN_USER}" \
+    --admin_password="${ADMIN_USER_PASS}" \
+    --admin_email="${ADMIN_USER_EMAIL}" \
+    --path="/wordpress"
+
+# # Create new user in WordPress
+# wp user create "${WP_USER}" \
+#     --user_pass="${WP_USER_PASSWORD}" \
+#     --role=author
 
 # Modifies the PHP-FPM configuration file to change the listen directive to use port 9000 instead of a Unix socket
 sed -i 's|listen = /run/php/php8.1-fpm.sock|listen = 9000|g' /etc/php81/php-fpm.d/www.conf
 sed -i 's|listen = 127.0.0.1:9000|listen = 0.0.0.0:9000|g' /etc/php81/php-fpm.d/www.conf
 
-
 exec "$@"
+
